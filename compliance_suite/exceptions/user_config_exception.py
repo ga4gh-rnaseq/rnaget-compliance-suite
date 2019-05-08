a6e6a84076a95c618e96737e0df3d08f7502088c
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-05-08 12:11:08 -0400

    library-specific exceptions

---
+++ compliance_suite/exceptions/user_config_exception.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Module compliance_suite.exceptions.user_config_exception.py
+
+This module contains class definition for user config file exceptions.
+"""
+
+class UserConfigException(Exception):
+    """Exception for user config file-related errors"""
+
+    pass
