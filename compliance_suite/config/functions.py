b83c0d16c8520b23f9bd4fba2dccc128b51db81e
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-07-03 11:03:05 -0400

    removed non-html files for gh-pages branch

--- compliance_suite/config/functions.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-from compliance_suite.config.constants import *
-from compliance_suite.config.tests import *
-
-def get_longest_testname_length():
-    return max([len(a) for a in TESTS_DICT.keys()])
--- compliance_suite/config/param_functions.py
+++
@@ -1,7 +0,0 @@
-import compliance_suite.config.constants as c
-
-def switch_format_param(params):
-    if params["format"].lower() == "loom":
-        params["format"] = "tsv"
-    else:
-        params["format"] = "loom"
