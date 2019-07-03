b95106d652597cd7961cc904f6126281b14c187e
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-07-03 14:02:25 -0400

    moved index.rst -> contents.rst for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Non-default settings
+master_doc = "contents"
+
+
 
 # -- Options for HTML output -------------------------------------------------
 
