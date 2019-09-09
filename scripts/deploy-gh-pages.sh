1eedb353cabeb92ae6fe46f4e6001be38219f260
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-09-09 15:06:15 -0400

    remove python 3.4, 3.5

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -4,6 +4,9 @@ set -e # exit with non-zero exit code if anything fails
 
 # only run script if travis branch is master
 # and this is NOT a PR
+echo "***"
+echo "$TRAVIS_PYTHON_VERSION"
+echo "***"
 if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
 # takes the reference implementation yaml file, and replaces placeholder tokens
